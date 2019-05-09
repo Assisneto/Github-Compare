14e73f0273e3613dd844597239673ed399bcb372
Assisneto <netodamasceno12@gmail.com>
2019-05-09 12:17:03 -0300

    Updates

---
+++ precache-manifest.9af2bd757c0c91cfd2fdedfe9f94fb20.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aa9c9d89b1ba17a6e4414ee67bedc9",
+    "url": "/Github-Compare/index.html"
+  },
+  {
+    "revision": "62c3814baa4bac673192",
+    "url": "/Github-Compare/static/css/2.ca128dd9.chunk.css"
+  },
+  {
+    "revision": "591e1ff87ed86228c95d",
+    "url": "/Github-Compare/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "62c3814baa4bac673192",
+    "url": "/Github-Compare/static/js/2.44103794.chunk.js"
+  },
+  {
+    "revision": "591e1ff87ed86228c95d",
+    "url": "/Github-Compare/static/js/main.ec5ae626.chunk.js"
+  },
+  {
+    "revision": "76735a2c09d6510508f3",
+    "url": "/Github-Compare/static/js/runtime~main.e20d4156.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Github-Compare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Github-Compare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Github-Compare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Github-Compare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Github-Compare/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
